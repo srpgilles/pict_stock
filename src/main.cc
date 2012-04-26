615bf83dc1c424de7fcbb895ea950b694ff596ea
Sebastien Gilles <srpgilles@gmail.com>
2012-04-26 20:39:44 +0200

    Quick and dirty way to provide input and output folders to the executable (but still better than hardcoded used previously)

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,36 @@
 #include "extended_photo.hpp"
 #include "sort_new_photos.hpp"
 
+namespace
+{
+	/*!
+	 * \brief read the parameter file and check the expected fields are there
+	 *
+	 * \param File Parameters file. It is expected to fill values inputFolder,
+	 * outputFolder and logFile
+	 * \param[out] parameters Parameters read
+
+	 * \return True if the value swere all correctly read
+	 */
+	bool readParameterFile(LoggingFacility& logs, const YString& file,
+			std::map<Yuni::CString<12, false>, YString>& parameters)
+	{
+		YString key, value;
+
+		if (!Yuni::IO::File::ReadLineByLine(file, [&] (const YString& line)
+		{
+			line.extractKeyValue(key, value, false);
+			parameters[key] = value;
+		}))
+		{
+			logs.error() << "Unable to read file " << file;
+			return false;
+		}
+
+		return true;
+	}
+
+} // namespace anonymous
 
 int main(int argc, char* const argv[])
 {
@@ -19,10 +49,41 @@ int main(int argc, char* const argv[])
 //
 //	return 0;
 
-	SgPhoto::PhotoDirectory photoDirectory(logs, "/tmp/devPhotos");
+	std::map<Yuni::CString<12, false>, YString> parameters;
 
-	SgPhoto::SortNewPhotos foo(logs, "/home/sebastien/Multimedia/Photos/A_classer",
-		photoDirectory, "/tmp/summary.txt", false);
+	if (!readParameterFile(logs, "parameters.ini", parameters))
+		exit(EXIT_FAILURE);
+
+	auto end = parameters.cend();
+
+	auto iter = parameters.find("inputFolder");
+	if (iter == end)
+	{
+		logs.fatal() << "inputFolder not read in parameter file";
+		exit(EXIT_FAILURE);
+	}
+	YString inputFolder(iter->second);
+
+	iter = parameters.find("outputFolder");
+	if (iter == end)
+	{
+		logs.fatal() << "outputFolder not read in parameter file";
+		exit(EXIT_FAILURE);
+	}
+	YString outputFolder(iter->second);
+
+	iter = parameters.find("logFile");
+	if (iter == end)
+	{
+		logs.fatal() << "logFile not read in parameter file";
+		exit(EXIT_FAILURE);
+	}
+	YString logFile(iter->second);
+
+
+	SgPhoto::PhotoDirectory photoDirectory(logs, outputFolder);
+
+	SgPhoto::SortNewPhotos foo(logs, inputFolder, photoDirectory, logFile, true);
 
 	if (!foo.proceed())
 		return EXIT_FAILURE;

423f3c5877687f4c470dc863dae39853de89c1a4
Sebastien Gilles <srpgilles@gmail.com>
2012-05-23 16:11:58 +0200

    Introduce a template specialization to be able to use either type or value in path format evaluation. It isn't very clean; Ill have toi find a better way to do that

--- src/private/traits/traits.hpp
+++ src/private/traits/traits.hpp
@@ -9,7 +9,7 @@ namespace Private
 {
 namespace Traits
 {
-	enum
+	enum EnumElements
 	{
 		feYear = 0,
 		feMonth,
@@ -24,11 +24,20 @@ namespace Traits
 	typedef Yuni::CString<4, false> SymbolString;
 	typedef Yuni::CString<30, true> RegexString;
 
+	template<EnumElements>
+	struct Element;
+
 	struct Year
 	{
 		static const SymbolString Symbol() { return "%y"; }
 		static const RegexString Regex() { return "(\\d{4})"; }
 		enum { index = feYear };
+	};
+
+	template<>
+	struct Element<feYear>
+	{
+		typedef Year Value;
 	};
 
 	struct Month
@@ -38,11 +47,23 @@ namespace Traits
 		enum { index = feMonth };
 	};
 
+	template<>
+	struct Element<feMonth>
+	{
+		typedef Month Value;
+	};
+
 	struct Day
 	{
 		static const SymbolString Symbol() { return "%d"; }
 		static const RegexString Regex() { return  "([ |0|1|2]\\d{1}|3[0|1])"; } // no check for adequacy month day (for instance 30 Feb is allowed)
 		enum { index = feDay };
+	};
+
+	template<>
+	struct Element<feDay>
+	{
+		typedef Day Value;
 	};
 
 	struct Hour
@@ -52,11 +73,24 @@ namespace Traits
 		enum { index = feHour };
 	};
 
+	template<>
+	struct Element<feHour>
+	{
+		typedef Hour Value;
+	};
+
 	struct Minute
 	{
 		static const SymbolString Symbol() { return "%M"; }
 		static const RegexString Regex() { return  "([ 0-5]\\d{1}|  )"; } // empty is allowed
 		enum { index = feMinute };
+	};
+
+
+	template<>
+	struct Element<feMinute>
+	{
+		typedef Minute Value;
 	};
 
 	struct Second
@@ -66,12 +100,24 @@ namespace Traits
 		enum { index = feSecond };
 	};
 
+	template<>
+	struct Element<feSecond>
+	{
+		typedef Second Value;
+	};
+
 
 	struct Photographer
 	{
 		static const SymbolString Symbol() { return "%P"; }
 		static const RegexString Regex() { return "(.*)"; }
 		enum { index = fePhotographer };
+	};
+
+	template<>
+	struct Element<fePhotographer>
+	{
+		typedef Photographer Value;
 	};
 
 } // namespace Traits

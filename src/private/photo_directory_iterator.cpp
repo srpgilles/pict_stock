638540db1e63f206c0e96f79f0954d1a210d4276
Sebastien Gilles <srpgilles@gmail.com>
2012-06-04 15:49:12 +0200

    Photo directory iterator now correctlyu written in terms of new class PathFormat

--- src/private/photo_directory_iterator.cpp
+++ src/private/photo_directory_iterator.cpp
@@ -67,72 +67,15 @@ namespace Private
 
 	bool PhotoDirectoryIterator::checkValidity(const String& directory)
 	{
-		std::map<Traits::Element::Ptr, Yuni::CString<10, false> > elements;
+		RelevantInformations usefulInformations(logs);
 
-		if (pPathFormat.doFolderMatch(directory, elements))
+		if (pPathFormat.doFolderMatch(directory, usefulInformations))
 		{
-			auto foo = pPathFormat.folderContent();
-
-			//RelevantInformations infos =
-			//pValidDirectories;
-			//pPathFormat.determineFolderKey(buf, )
-			//pValidDirectories.insert(make_pair(pPathFormat.));
+			pValidDirectories.insert(std::make_pair(usefulInformations, directory));
+			return true;
 		}
 
-		// TODO Complete!
-
-
-		return true;
-
-//		// Split the path in subfolders to see if they are legit directories
-//		std::vector<String> subPath;
-//		directory.split(subPath, IO::Separator);
-//
-//		String folderDate;
-//
-//		auto it = subPath.cbegin();
-//		auto end = subPath.cend();
-//
-//
-//		// Here we have found nothing so far, and are looking
-//		// for year element. Year element is just an integer in
-//		// complete form
-//		{
-//			bool found = false;
-//			for (; !found && it != end ; ++it)
-//				found = isNumericDate(folderDate, *it, YearTraits::Min, YearTraits::Max);
-//
-//			if (it == end)
-//				return false;
-//		}
-//
-//		// Now we want to complete the month. Current accepted format is Mxx or
-//		// Mxx_* (but not Mxx-*; M04-05 is for instance not valid and should be
-//		// completed by subfolders M04 and M05 inside)
-//		// As of this writing, C++11 regexp not supported by g++, so I'll do
-//		// it more manually (this should be changed for more flexibility later)
-//		{
-//			bool found = false;
-//
-//			for (; !found && it != end ; ++it)
-//				found = monthOrDayHelper<MonthTraits>(folderDate, *it);
-//
-//			if (it == end)
-//				return false;
-//		}
-//
-//		// Do the same stuff for day
-//		{
-//			bool found = false;
-//
-//			for (; !found && it != end ; ++it)
-//				found = monthOrDayHelper<DayTraits>(folderDate, *it);
-//
-//			if (found)
-//				pValidDirectories[folderDate].push_back(directory);
-//
-//			return found;
-//		}
+		return false;
 	}
 
 

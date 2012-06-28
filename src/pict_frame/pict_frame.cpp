4efcf0d8ab7af0cf0fe58bf9fd6a6b80a388bd63
Sebastien Gilles <srpgilles@gmail.com>
2012-06-28 22:29:35 +0200

    PictFrame: move into anonymous namespace part of the code that is essentially plumbery to choose a subset of photos

--- src/pict_frame/pict_frame.cpp
+++ src/pict_frame/pict_frame.cpp
@@ -24,6 +24,74 @@ namespace PictStock
 			return (infos1Ptr->date() < infos2Ptr->date());
 		}
 
+
+		/*!
+		 ** \brief Select a semi-random subset of indexes
+		 **
+		 ** "Semi-random" because the subset is almost homogeneously parted between
+		 ** the whole interval
+		 ** "Almost homogeneously" because we respect the exact number of indexes
+		 ** requested and therefore delete a few if sequence generated was too long
+		 **
+		 ** \param[out] out Subset generated
+		 ** \param[in] poolSize Size of the original pool from which subset will be formed
+		 ** \param[in] subsetSize Requested size for the subset. Must be strictly lower
+		 ** than poolSize (other cases should have been handled in another part of the code)
+		 **
+		 ** Algorithm
+		 **  . Calculate the distance
+		 **  . Generate a random number in [0, distance[ to determine the starting
+		 **  point of the sequence
+		 **  . If the sequence is longer than the requested number of elements,
+		 **  eliminate randomly as many elements as necessary. To do so, perform
+		 **  shuffle and then delete last entries for the sake of efficiency
+		 */
+		void semiRandomSubset(std::vector<unsigned int>& out, unsigned int poolSize,
+			unsigned int subsetSize)
+		{
+			// Calculate the "distance" between two photos to be kept
+			// For instance, if we want 3 photos out of 10, distance is 10/3 = 3
+			unsigned int dist = static_cast<unsigned int>(poolSize) / subsetSize;
+
+			// Generate a random number in [0, distance[ to determine the starting
+			// point of the sequence; generate from there the sequence of indexes to
+			// consider
+
+			{
+				out.reserve(1u + poolSize / dist); // upper bound of maximum size
+				Math::Random::Default random;
+				unsigned int startingIndex = static_cast<unsigned int>(random.next()) % dist;
+
+				for (unsigned int i = startingIndex; i < poolSize; i += dist)
+					out.push_back(i);
+			}
+
+
+			{
+				// If too many elements, shuffle the list and remove as many last elements as
+				// necessary
+				unsigned int nbElementsToDelete;
+
+				{
+					unsigned int lengthSeq = static_cast<unsigned int>(out.size());
+					assert(lengthSeq >= subsetSize && "If not, bug somewhere in PictFrame::selectPhotos");
+					nbElementsToDelete = lengthSeq - subsetSize;
+				}
+
+				if (nbElementsToDelete > 0u)
+				{
+					std::random_shuffle(out.begin(), out.end());
+					while (nbElementsToDelete-- > 0u)
+						out.pop_back();
+				}
+
+				assert(out.size() == subsetSize);
+			}
+		}
+
+
+
+
 	} // namespace anonymous
 
 
@@ -69,60 +137,10 @@ namespace PictStock
 		}
 		else
 		{
-			/*
-			 *  Algorithm to implement:
-			 *
-			 *  . Calculate the distance
-			 *  . Generate a random number in [0, distance[ to determine the starting
-			 *  point of the sequence
-			 *  . If the sequence is longer than the requested number of elements,
-			 *  eliminate randomly as many elements as necessary. To do so, perform
-			 *  shuffle and then delete last entries for the sake of efficiency
-			 *  . And if user want chronological order, it is not a big deal to sort
-			 *  again the vector with a proper predicate
-			 *
-			 *
-			 */
-
-			// Calculate the "distance" between two photos to be kept
-			// For instance, if we want 3 photos out of 10, distance is 10/3 = 3
-			unsigned int dist = static_cast<unsigned int>(poolSize) / pNbPhotos;
-
-			// Generate a random number in [0, distance[ to determine the starting
-			// point of the sequence; generate from there the sequence of indexes to
-			// consider
 			std::vector<unsigned int> randomSequence;
 
-			{
-				randomSequence.reserve(1u + poolSize / dist); // upper bound of maximum size
-				Math::Random::Default random;
-				unsigned int startingIndex = static_cast<unsigned int>(random.next()) % dist;
-
-				for (unsigned int i = startingIndex; i < poolSize; i += dist)
-					randomSequence.push_back(i);
-			}
-
-
-			{
-				// If too many elements, shuffle the list and remove as many last elements as
-				// necessary
-				unsigned int nbElementsToDelete;
-
-				{
-					unsigned int lengthSeq = static_cast<unsigned int>(randomSequence.size());
-					assert(lengthSeq >= pNbPhotos && "If not, bug somewhere in PictFrame::selectPhotos");
-					nbElementsToDelete = lengthSeq - pNbPhotos;
-				}
-
-				if (nbElementsToDelete > 0u)
-				{
-					std::random_shuffle(randomSequence.begin(), randomSequence.end());
-					while (nbElementsToDelete-- > 0u)
-						randomSequence.pop_back();
-				}
-
-				assert(randomSequence.size() == pNbPhotos);
-			}
+			// Select semi-randomly a subset of indexes between 0 and poolSize - 1
+			semiRandomSubset(randomSequence, poolSize, pNbPhotos);
 
 			{
 				// Finally create the list from the index generated above
